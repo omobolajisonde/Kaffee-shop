215e8bdd1b20178d2de77dcce3695006c992bd26
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2022-09-14 18:29:07 +0100

    Sorted out some submission related issues

--- backend/src/api.py
+++ backend/src/api.py
@@ -24,7 +24,7 @@ def after_request(response):
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
 
